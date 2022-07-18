274b95e756906e2917d431215850a3d95df711c2
Lavish-703 <82791010+Lavish-703@users.noreply.github.com>
2022-07-18 10:16:16 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+$("#product-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  // nav: true,
+  // autoplay: true,
+  // autoplayTimeout: 1000,
+  // autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
